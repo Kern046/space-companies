3aca2e7976fc9a6ba536073faa41f0b2cce338bd
Axel Venet <avenet@wamiz.com>
2021-12-20 01:48:13 +0100

    Replace route annotations with YAML configuration

--- src/Shared/Infrastructure/Controller/HomepageController.php
+++ src/Shared/Infrastructure/Controller/HomepageController.php
@@ -5,11 +5,9 @@ namespace App\Shared\Infrastructure\Controller;
 use App\Company\Infrastructure\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 final class HomepageController extends AbstractController
 {
-    #[Route("/", name: "homepage", methods: ["GET"])]
     public function __invoke(CompanyRepository $companyRepository): Response
     {
         return $this->render('pages/homepage.html.twig', [
